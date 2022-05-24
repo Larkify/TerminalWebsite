bf24691542f1a4c0b0758a1133f1d77014116a71
Kiran <contact@larkx.xyz>
2022-05-24 18:20:18 +0100

    Update Ps1.tsx

--- src/components/Ps1.tsx
+++ src/components/Ps1.tsx
@@ -4,11 +4,11 @@ import config from '../../config.json';
 export const Ps1 = () => {
   return (
     <div>
-      <span className="text-light-yellow dark:text-dark-yellow">
+      <span style="color: #f14d50" className="text-light-yellow dark:text-dark-yellow">
         {config.ps1_username}
       </span>
       <span className="text-light-gray dark:text-dark-gray">@</span>
-      <span className="text-light-green dark:text-dark-green">
+      <span style="color: #2392f1" className="text-light-green dark:text-dark-green">
         {config.ps1_hostname}
       </span>
       <span className="text-light-gray dark:text-dark-gray">:$ ~ </span>
