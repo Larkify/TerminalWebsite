e1ccdc495acf0c411d8e54b8bd91c190e2f44941
Kiran <contact@larkx.xyz>
2022-05-24 17:27:38 +0100

    commit

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -11,21 +11,14 @@ export const help = async (args: string[]): Promise<string> => {
     if (i % 7 === 0) {
       c += Object.keys(bin).sort()[i - 1] + '\n';
     } else {
-      c += Object.keys(bin).sort()[i - 1] + ' ';
+      c += Object.keys(bin).sort()[i - 1] + ' ,';
     }
   }
   return `Welcome! Here are all the available commands:
 \n${c}\n
 [tab]: trigger completion.
 [ctrl+l]/clear: clear terminal.\n
-Type 'sumfetch' to display summary.
 `;
-};
-
-// Redirection
-export const repo = async (args: string[]): Promise<string> => {
-  window.open(`${config.repo}`);
-  return 'Opening Github repository...';
 };
 
 // About
@@ -38,20 +31,6 @@ More about me:
 'readme' - my github readme.`;
 };
 
-export const resume = async (args: string[]): Promise<string> => {
-  window.open(`${config.resume_url}`);
-  return 'Opening resume...';
-};
-
-// Donate
-export const donate = async (args: string[]): Promise<string> => {
-  return `thank you for your interest. 
-here are the ways you can support my work:
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
-`;
-};
-
 // Contact
 export const email = async (args: string[]): Promise<string> => {
   window.open(`mailto:${config.email}`);
@@ -60,35 +39,23 @@ export const email = async (args: string[]): Promise<string> => {
 
 export const github = async (args: string[]): Promise<string> => {
   window.open(`https://github.com/${config.social.github}/`);
-
   return 'Opening github...';
 };
 
-export const linkedin = async (args: string[]): Promise<string> => {
-  window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`);
+export const youtube = async (args: string[]): Promise<string> => {
+  window.open(`https://youtube.com/${config.social.youtube}/`);
+  return 'Opening youtube...';
+};
 
-  return 'Opening linkedin...';
+export const discord = async (args: string[]): Promise<string> => {
+  window.open(`https://discord.gg/${config.social.discord}/`);
+  return 'Opening discord...';
 };
 
 // Search
 export const google = async (args: string[]): Promise<string> => {
   window.open(`https://google.com/search?q=${args.join(' ')}`);
   return `Searching google for ${args.join(' ')}...`;
-};
-
-export const duckduckgo = async (args: string[]): Promise<string> => {
-  window.open(`https://duckduckgo.com/?q=${args.join(' ')}`);
-  return `Searching duckduckgo for ${args.join(' ')}...`;
-};
-
-export const bing = async (args: string[]): Promise<string> => {
-  window.open(`https://bing.com/search?q=${args.join(' ')}`);
-  return `Wow, really? You are using bing for ${args.join(' ')}?`;
-};
-
-export const reddit = async (args: string[]): Promise<string> => {
-  window.open(`https://www.reddit.com/search/?q=${args.join(' ')}`);
-  return `Searching reddit for ${args.join(' ')}...`;
 };
 
 // Typical linux commands
@@ -147,7 +114,7 @@ export const banner = (args?: string[]): string => {
   ███████║█████╗░░░╚████╔╝░░░░    ██║░╚╝██╔████╔██║    █████═╝░██║██████╔╝███████║██╔██╗██║
   ██╔══██║██╔══╝░░░░╚██╔╝░░██╗    ██║░░░██║╚██╔╝██║    ██╔═██╗░██║██╔══██╗██╔══██║██║╚████║
   ██║░░██║███████╗░░░██║░░░╚█║    ██║░░░██║░╚═╝░██║    ██║░╚██╗██║██║░░██║██║░░██║██║░╚███║
-  ╚═╝  ╚═╝╚══════╝░░░╚═╝░░░░╚╝    ╚═╝░░░╚═╝░░░░░╚═╝    ╚═╝░░╚═╝╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝
+  ╚═╝  ╚═╝╚══════╝   ╚═╝    ╚╝    ╚═╝   ╚═╝     ╚═╝    ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚══╝
 
 Type 'help' to see the list of available commands.
 Type 'sumfetch' to display summary.
